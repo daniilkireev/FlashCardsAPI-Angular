f265c7c6ddadde4afef5615b91c7c6f40f404c39
daniilkireev <daniilkireev@mail.ru>
2022-04-04 23:26:30 +0300

    Added InspectionTypes table view

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspectionList$!:Observable<any[]>;
+  inspectionTypesList$!:Observable<any[]>;
+  inspectionTypesList:any=[];
+
+  // Map to display data associate with foreing keys
+  inspectiontypesMap:Map<number, string> = new Map()
+
+  constructor(private service:InspectionApiService) { }
 
   ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
   }
 
 }
